4485810b4b62b5ce4bbcdc42a5b2c8a43a2f41db
artiskatwit <artisk@wit.edu>
2025-06-17 22:58:48 -0400

    Completion of profileactivity.xml Start of ProfileActivity.java

---
+++ app/src/main/java/main/ProfileActivity.java
@@ -0,0 +1,19 @@
+package main;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private UserProfileManager userProfileManager;
+
+
+}
