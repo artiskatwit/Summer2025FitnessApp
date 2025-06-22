e2efb6d1b7e57c3624ed92bcca1940f8768af45c
artiskatwit <artisk@wit.edu>
2025-06-21 22:12:41 -0400

    LoginActivity in progress,FireBase added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
+
 }
 
 android {
@@ -37,7 +39,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0")) // Check for the latest BoM version
+
+
+    implementation("com.google.firebase:firebase-auth")
 }
